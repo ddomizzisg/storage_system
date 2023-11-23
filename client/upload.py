e4ebb7bfb6c3f785bea37cd29be439285b823cf0
ddomizzisg <ddomizzisag@gmail.com>
2023-11-23 20:47:06 +0100

    modified

--- client/upload.py
+++ client/upload.py
@@ -22,9 +22,15 @@ class RunStats(NamedTuple):
     median: float
     stdev_time_ms: float
     avg_bandwidth_mbps: float | None
+    avg_upload_time: float
+    avg_server_time: float
+    avg_response_time: float
 
 def upload_file( url, payload_size=1000000):
     times_ms = []
+    upload_times = []
+    server_times = []
+    response_times = []
 
     file_path = '%d' % payload_size 
     data = randbytes(payload_size)
@@ -38,10 +44,31 @@ def upload_file( url, payload_size=1000000):
         print(file)
         for i in range(10):
             start = time.perf_counter_ns()
+            #get current timestamp in miliseconds
+            timestamp_up_start = int(time.time() * 1000)
             response = requests.post(url, files={'file': data}, data={'name': file_path})
-            print(response.text)
+            #print(response.text)
+            
+
             if response.status_code == 200:
-                print("File uploaded successfully!")
+                timestamp_up_end = int(time.time() * 1000)
+                result = response.json()
+                if(result['code'] == 0):
+                    print("File uploaded successfully!")
+                    server_start = result['time_start']
+                    server_end = result['time_end']
+                    timezoneserver = result['timezone']
+                    #print(timestamp_up_start,server_start, timezoneserver)
+                    #print(timestamp_up_end,server_end)
+                    upload_time = server_start - timestamp_up_start
+                    server_time = server_end - server_start
+                    response_time = timestamp_up_end - server_end
+                    upload_times.append(upload_time)
+                    server_times.append(server_time)
+                    response_times.append(response_time)
+
+                else:
+                    print("File upload failed.")
             else:
                 print("File upload failed.")
             end = time.perf_counter_ns()
@@ -50,7 +77,10 @@ def upload_file( url, payload_size=1000000):
     avg_time_s = sum(times_ms) / 1000 / len(times_ms)
     payload_mb = payload_size / 1e6
     avg_bandwidth_mbps = payload_mb / avg_time_s
-    print(avg_time_s,payload_mb,avg_bandwidth_mbps)
+    avg_upload_time = sum(upload_times) / len(upload_times)
+    avg_server_time = sum(server_times) / len(server_times)
+    avg_response_time = sum(response_times) / len(response_times)
+    #print(avg_time_s,payload_mb,avg_bandwidth_mbps)
 
     return RunStats(
         payload_size_bytes=payload_size,
@@ -63,15 +93,18 @@ def upload_file( url, payload_size=1000000):
             statistics.stdev(times_ms) if len(times_ms) > 1 else 0.0
         ),
         avg_bandwidth_mbps=avg_bandwidth_mbps,
+        avg_upload_time=avg_upload_time,
+        avg_server_time=avg_server_time,
+        avg_response_time=avg_response_time,
     )
 
 # Example usage
 payload_size = int(sys.argv[1])
 file_path = 'test.txt'
-url = 'http://54.85.1.57:20006/set.php?file=%s' % file_path  # Replace with the PHP service URL
+url = 'http://54.85.179.6:20006/set.php?file=%s' % file_path  # Replace with the PHP service URL
 run_stats = upload_file(url, payload_size=payload_size)
 logger.log(TESTING_LOG_LEVEL, run_stats)
 
-csv_logger = CSVLogger("results_upload.csv", RunStats)
+csv_logger = CSVLogger("results_upload_new.csv", RunStats)
 csv_logger.log(run_stats)
 csv_logger.close()
--- node/set.php
+++ node/set.php
@@ -1,6 +1,10 @@
 <?php
 
 #print_r($_FILES);
+$tz = 'Indian/Mahe';   
+date_default_timezone_set($tz);
+//Get current timestamp in milliseconds
+$time_start = microtime(true) * 1000;
 
 // Check if a file was uploaded successfully
 if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
@@ -15,10 +19,18 @@ if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
 
     // Write the file data to the destination directory
     if (move_uploaded_file($file['tmp_name'], $destinationDir . $filename)) {
-        echo 'File uploaded successfully.';
+        #echo 'File uploaded successfully.';
+        //Get current timestamp in milliseconds
+        $time_end = microtime(true) * 1000;
+
+        $result = array("code" => 0,"time_start" => $time_start, "time_end" => $time_end, "time" => $time_end - $time_start, "timezone" => date_default_timezone_get());
+        echo json_encode($result);
+
     } else {
-        echo 'Failed to upload file.';
+        #echo 'Failed to upload file.';
+        $result = array("code" => 1);
     }
 } else {
     echo 'No file uploaded or an error occurred during upload.';
+    $result = array("code" => 1);
 }
