efa2d782f86feb23906d949517e11d88141a794f
ddomizzisg <ddomizzisag@gmail.com>
2024-05-18 17:16:58 +0200

    changes

--- node/clear_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Borrar archivos y keys
-find /var/www/html/abekeys/ ! -name 'upload.php' -type f -exec rm -f {} +
-find /var/www/html/c/ ! -name 'upload.php' -type f -exec rm -f {} +
-
